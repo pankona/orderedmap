94c8e9c69ae9eaea3db138a6e36fa664f9d75c38
pankona <yosuke.akatsuka@gmail.com>
2018-10-07 22:56:51 +0900

    supply more test code

--- orderedmap_test.go
+++ orderedmap_test.go
@@ -37,13 +37,32 @@ func TestInsert(t *testing.T) {
 		require.Equal(t, om.Len(), i+1)
 	}
 
+	// insert middle
 	v1 := keyable(500)
 	err := om.Insert(v1, 50)
 	require.Nil(t, err, "failed to insert")
+	require.Equal(t, om.Len(), 101)
 
 	v2 := om.GetByIndex(50)
 	require.Equal(t, v1, v2)
-	require.Equal(t, om.Len(), 101)
+
+	// insert top
+	v1 = keyable(501)
+	err = om.Insert(v1, 0)
+	require.Nil(t, err, "failed to insert")
+	require.Equal(t, om.Len(), 102)
+
+	v2 = om.GetByIndex(0)
+	require.Equal(t, v1, v2)
+
+	// insert tail
+	v1 = keyable(502)
+	err = om.Insert(v1, om.Len())
+	require.Nil(t, err, "failed to insert")
+	require.Equal(t, om.Len(), 103)
+
+	v2 = om.GetByIndex(om.Len() - 1)
+	require.Equal(t, v1, v2)
 }
 
 func TestUpdate(t *testing.T) {
